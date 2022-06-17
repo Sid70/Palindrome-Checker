d6640a69eddaed066f21c6f30f5b4e1c0770d8b5
Siddhanta Choudhury <103874320+Sid70@users.noreply.github.com>
2022-06-17 10:49:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+function palindrome() {
+
+    let str = document.getElementById('Word').value;
+    str = str.toLowerCase();// temp is a string (in lowercase)  
+    let k = str.length; //string length
+    let count = 0;
+    let remove_punctuation = "";
+    let temp;
+
+
+    str = str.replace(/[., \/#!$%\^&\*;:{}=\-_`~()]/g, remove_punctuation);
+    k = str.length;
+
+    temp = str;
+
+    //check palindrome
+    for (let i = 0; i < str.length; i++, k--) {
+        if (temp[k - 1] == str[i]) {
+            count++;
+        }
+    }
+
+    if (count == temp.length) {
+       document.getElementById('Throw').innerHTML = "Yes, '" + str + " ' is a Palindrome.";
+    }
+
+    else {
+        document.getElementById('Throw').innerHTML = "No, '" + str + " ' is not a Palindrome.";
+    }
+}
+
